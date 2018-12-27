aa22f7f26aa05cab13d40e020294bd975c5b22fe
AntaresQlapard <46163003+AntaresQlapard@users.noreply.github.com>
2018-12-27 19:06:16 +0600

    Update antaresqlapard.py

--- antaresqlapard.py
+++ antaresqlapard.py
@@ -16,6 +16,7 @@ async def on_messsage(message):
     if message.content.startswith("?hi"):
         msg = 'Hello (0.author.mention) How are you?'.format(message)
         await client.send_message(message.channel)
+	
 client.run(rafsanrahman)
 
 	
